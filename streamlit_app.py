3d32f288405e108bb43d0c402c972cfc4d5f1913
t.busch <t.busch@fz-juelich.de>
2022-03-16 16:53:06 +0100

    automated commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,6 @@ width = 3000 / scale
 height = 1000 / scale
 
 
-
 def annual_mean(df, scalefac):
     annual_mean_val = df.mean() * 365 / scalefac
     annual_mean_val = int(round(annual_mean_val, 0))
@@ -63,12 +62,6 @@ st.markdown("# Energy imports from Russia and possible alternatives")
 
 st.markdown("## Scenario calculation: Reduction of Russian gas imports")
 
-st.markdown("Running scenario...")
-opti.run_scenario()
-
-
-
-
 cols = st.columns(4)
 pl_reduction = cols[0].selectbox("Anteil russischer Gas-Importe [%]", [0])
 
@@ -80,274 +73,283 @@ lng_capacity = cols[2].selectbox(
 
 soc_slack = cols[3].selectbox("SOC Slack", ["False", "True"], 0)
 
-df = get_optiRes(pl_reduction, lng_capacity, reduced_demand, soc_slack)
+start_opti = st.button("Start optimization")
 
-# cols = st.columns(2)
-
-# Demand
-total_demand = df.dom_Dem + df.elec_Dem + df.ind_Dem + df.ghd_Dem + df.exp_n_oth
-total_demand_served = (
-    df.dom_served + df.elec_served + df.ind_served + df.ghd_served + df.exp_n_oth_served
-)
-fig = go.Figure()
-xvals = df.time
+if start_opti:
+    with st.spinner(text="Running optimization..."):
+        opti.run_scenario()
 
 
-fig.add_trace(
-    go.Scatter(
-        x=xvals,
-        y=df.dom_served,
-        stackgroup="one",
-        # legendgroup="bedarf",
-        name="Haushalte",
-        mode="none",
-        fillcolor=FZJcolor.get("green")
-        # marker=marker_dict,
-    )
-)
 
-fig.add_trace(
-    go.Scatter(
-        x=xvals,
-        y=df.ghd_served,
-        stackgroup="one",
-        # legendgroup="bedarf",
-        name="GHD",
-        mode="none",
-        fillcolor=FZJcolor.get("purple2")
-    )
-)
+    with st.spinner(text="Fetching results..."):
+        df = get_optiRes(pl_reduction, lng_capacity, reduced_demand, soc_slack)
 
-fig.add_trace(
-    go.Scatter(
-        x=xvals,
-        y=df.elec_served,
-        stackgroup="one",
-        # legendgroup="bedarf",
-        name="Energie",
-        mode="none",
-        fillcolor=FZJcolor.get("blue")
-    )
-)
+        # cols = st.columns(2)
 
-fig.add_trace(
-    go.Scatter(
-        x=xvals,
-        y=df.ind_served,
-        stackgroup="one",
-        # legendgroup="bedarf",
-        # legendgrouptitle_text="Erdgasbedarfe",
-        name="Industrie",
-        mode="none",
-        fillcolor=FZJcolor.get("grey2"),
-    )
-)
-
-fig.add_trace(
-    go.Scatter(
-        x=xvals,
-        y=df.exp_n_oth_served,
-        stackgroup="one",
-        # legendgroup="bedarf",
-        name="Export und sonstige",
-        mode="none",
-        fillcolor=FZJcolor.get("blue2")
-    )
-)
-
-unserved_demand = total_demand - total_demand_served
-st.markdown(f"Abgeregelte Menge Erdgas: {int(sum(unserved_demand))} TWh")
-
-if sum(unserved_demand) > 0.001:
-    fig.add_trace(
-        go.Scatter(
-            x=xvals,
-            y=total_demand - total_demand_served,
-            stackgroup="one",
-            # legendgroup="bedarf",
-            name="Ungedeckter Bedarf",
-            mode="none",
-            fillcolor=FZJcolor.get("red"),
+        # Demand
+        total_demand = df.dom_Dem + df.elec_Dem + df.ind_Dem + df.ghd_Dem + df.exp_n_oth
+        total_demand_served = (
+            df.dom_served + df.elec_served + df.ind_served + df.ghd_served + df.exp_n_oth_served
         )
-    )
-
-fig.add_trace(
-    go.Scatter(
-        x=xvals,
-        y=df.lngServed,  # lng_val / 24,
-        stackgroup="two",
-        line=dict(color=FZJcolor.get("yellow3"), width=3.5),
-        # legendgroup="import",
-        # legendgrouptitle_text="Erdgasimport",
-        name="LNG Import",
-        fillcolor="rgba(0, 0, 0, 0)",
-    )
-)
-
-fig.add_trace(
-    go.Scatter(
-        x=xvals,
-        y=df.pipeServed,
-        stackgroup="two",
-        line=dict(color=FZJcolor.get("orange"), width=3.5),
-        # legendgroup="import",
-        name="Pipeline Import",
-        fillcolor="rgba(0, 0, 0, 0)",
-    )
-)
+        fig = go.Figure()
+        xvals = df.time
 
 
-fig.update_layout(
-    title=f"Erdgasbedarfe und Import",
-    font=font_dict,
-    yaxis_title="Erdgas [TWh/h]",
-    legend=legend_dict,
-)
-fig.update_layout(showlegend=False)
+        fig.add_trace(
+            go.Scatter(
+                x=xvals,
+                y=df.dom_served,
+                stackgroup="one",
+                # legendgroup="bedarf",
+                name="Haushalte",
+                mode="none",
+                fillcolor=FZJcolor.get("green")
+                # marker=marker_dict,
+            )
+        )
 
-if write_image:
-    fig.write_image(
-        f"Output/Optimierung_Erdgasbedarf_{pl_reduction}_{lng_capacity}_{reduced_demand}_{soc_slack}.png",
-        width=width,
-        height=height,
-        # scale=scale,
-    )
+        fig.add_trace(
+            go.Scatter(
+                x=xvals,
+                y=df.ghd_served,
+                stackgroup="one",
+                # legendgroup="bedarf",
+                name="GHD",
+                mode="none",
+                fillcolor=FZJcolor.get("purple2")
+            )
+        )
 
-st.plotly_chart(fig, use_container_width=True)
+        fig.add_trace(
+            go.Scatter(
+                x=xvals,
+                y=df.elec_served,
+                stackgroup="one",
+                # legendgroup="bedarf",
+                name="Energie",
+                mode="none",
+                fillcolor=FZJcolor.get("blue")
+            )
+        )
 
+        fig.add_trace(
+            go.Scatter(
+                x=xvals,
+                y=df.ind_served,
+                stackgroup="one",
+                # legendgroup="bedarf",
+                # legendgrouptitle_text="Erdgasbedarfe",
+                name="Industrie",
+                mode="none",
+                fillcolor=FZJcolor.get("grey2"),
+            )
+        )
 
-#%%
-# SOC
-fig = go.Figure()
+        fig.add_trace(
+            go.Scatter(
+                x=xvals,
+                y=df.exp_n_oth_served,
+                stackgroup="one",
+                # legendgroup="bedarf",
+                name="Export und sonstige",
+                mode="none",
+                fillcolor=FZJcolor.get("blue2")
+            )
+        )
 
-fig.add_trace(
-    go.Scatter(
-        x=xvals,
-        y=df.soc,
-        stackgroup="one",
-        name="Füllstand",
-        mode="none",
-        fillcolor=FZJcolor.get("orange")
-    )
-)
+        unserved_demand = total_demand - total_demand_served
+        st.markdown(f"Abgeregelte Menge Erdgas: {int(sum(unserved_demand))} TWh")
 
-fig.add_trace(
-    go.Line(
-        x=xvals,
-        y=np.ones(len(xvals)) * 1100,
-        name="Maximale Kapazität",
-        line_color=FZJcolor.get("black")
-    )
-)
+        if sum(unserved_demand) > 0.001:
+            fig.add_trace(
+                go.Scatter(
+                    x=xvals,
+                    y=total_demand - total_demand_served,
+                    stackgroup="one",
+                    # legendgroup="bedarf",
+                    name="Ungedeckter Bedarf",
+                    mode="none",
+                    fillcolor=FZJcolor.get("red"),
+                )
+            )
 
-fig.update_layout(
-    title=f"Speicherfüllstand",
-    font=font_dict,
-    yaxis_title="Erdgas [TWh]",
-    legend=legend_dict,
-)
-# fig.update_layout(showlegend=False)
+        fig.add_trace(
+            go.Scatter(
+                x=xvals,
+                y=df.lngServed,  # lng_val / 24,
+                stackgroup="two",
+                line=dict(color=FZJcolor.get("yellow3"), width=3.5),
+                # legendgroup="import",
+                # legendgrouptitle_text="Erdgasimport",
+                name="LNG Import",
+                fillcolor="rgba(0, 0, 0, 0)",
+            )
+        )
 
-if write_image:
-    fig.write_image(
-        f"Output/Optimierung_Speicher_{pl_reduction}_{lng_capacity}_{reduced_demand}_{soc_slack}.png",
-        width=width,
-        height=height,
-        # scale=scale,
-    )
-
-st.plotly_chart(fig, use_container_width=True)
-
-
-#%%
-# Pipeline Import
-fig = go.Figure()
-
-fig.add_trace(
-    go.Scatter(
-        x=xvals,
-        y=df.pipeServed,
-        stackgroup="one",
-        name="Pipeline Import",
-        mode="none",
-        fillcolor=FZJcolor.get("blue3")
-    )
-)
-
-fig.add_trace(
-    go.Scatter(
-        x=xvals,
-        y=df.lngServed,  # lng_val / 24,
-        stackgroup="one",
-        name="LNG Import",
-        mode="none",
-        fillcolor=FZJcolor.get("yellow")
-    )
-)
-
-fig.update_layout(
-    title=f"Erdgasimporte",
-    font=dict(size=16),
-    yaxis_title="Erdgas [TWh/h]",
-    legend=legend_dict,
-)
-st.plotly_chart(fig, use_container_width=True)
-
-
-#%%
-# Storage Charge and discharge
-storage_operation = df.lngServed + df.pipeServed - total_demand_served
-storage_discharge = [min(0, x) for x in storage_operation]
-storage_charge = np.array([max(0, x) for x in storage_operation])
-
-storage_operation_pl = df.pipeServed - total_demand_served
-storage_charge_pl = np.array([max(0, x) for x in storage_operation_pl])
-
-storage_operation_lng = storage_charge - storage_charge_pl
-# storage_operation_lng = df.pipeServed - total_demand_served
-storage_charge_lng = np.array([max(0, x) for x in storage_operation_lng])
+        fig.add_trace(
+            go.Scatter(
+                x=xvals,
+                y=df.pipeServed,
+                stackgroup="two",
+                line=dict(color=FZJcolor.get("orange"), width=3.5),
+                # legendgroup="import",
+                name="Pipeline Import",
+                fillcolor="rgba(0, 0, 0, 0)",
+            )
+        )
 
 
-fig = go.Figure()
+        fig.update_layout(
+            title=f"Erdgasbedarfe und Import",
+            font=font_dict,
+            yaxis_title="Erdgas [TWh/h]",
+            legend=legend_dict,
+        )
+        fig.update_layout(showlegend=False)
 
-fig.add_trace(
-    go.Scatter(
-        x=xvals,
-        y=storage_discharge,
-        stackgroup="two",
-        name="Ausspeicherung",
-        mode="none",
-        fillcolor=FZJcolor.get("red")
-    )
-)
+        if write_image:
+            fig.write_image(
+                f"Output/Optimierung_Erdgasbedarf_{pl_reduction}_{lng_capacity}_{reduced_demand}_{soc_slack}.png",
+                width=width,
+                height=height,
+                # scale=scale,
+            )
 
-fig.add_trace(
-    go.Scatter(
-        x=xvals,
-        y=storage_charge_pl,
-        stackgroup="one",
-        name="Speicherung (Pipeline)",
-        mode="none",
-        fillcolor=FZJcolor.get("yellow")
-    )
-)
+        st.plotly_chart(fig, use_container_width=True)
 
-fig.add_trace(
-    go.Scatter(
-        x=xvals,
-        y=storage_charge_lng,
-        stackgroup="one",
-        name="Speicherung (LNG)",
-        mode="none",
-        fillcolor=FZJcolor.get("blue3")
-    )
-)
 
-fig.update_layout(
-    title=f"Ein- und Ausspeicherung Gasspeicher",
-    font=dict(size=16),
-    yaxis_title="Erdgas [TWh/h]",
-    legend=legend_dict,
-)
+        #%%
+        # SOC
+        fig = go.Figure()
 
-st.plotly_chart(fig, use_container_width=True)
+        fig.add_trace(
+            go.Scatter(
+                x=xvals,
+                y=df.soc,
+                stackgroup="one",
+                name="Füllstand",
+                mode="none",
+                fillcolor=FZJcolor.get("orange")
+            )
+        )
+
+        fig.add_trace(
+            go.Line(
+                x=xvals,
+                y=np.ones(len(xvals)) * 1100,
+                name="Maximale Kapazität",
+                line_color=FZJcolor.get("black")
+            )
+        )
+
+        fig.update_layout(
+            title=f"Speicherfüllstand",
+            font=font_dict,
+            yaxis_title="Erdgas [TWh]",
+            legend=legend_dict,
+        )
+        # fig.update_layout(showlegend=False)
+
+        if write_image:
+            fig.write_image(
+                f"Output/Optimierung_Speicher_{pl_reduction}_{lng_capacity}_{reduced_demand}_{soc_slack}.png",
+                width=width,
+                height=height,
+                # scale=scale,
+            )
+
+        st.plotly_chart(fig, use_container_width=True)
+
+
+        #%%
+        # Pipeline Import
+        fig = go.Figure()
+
+        fig.add_trace(
+            go.Scatter(
+                x=xvals,
+                y=df.pipeServed,
+                stackgroup="one",
+                name="Pipeline Import",
+                mode="none",
+                fillcolor=FZJcolor.get("blue3")
+            )
+        )
+
+        fig.add_trace(
+            go.Scatter(
+                x=xvals,
+                y=df.lngServed,  # lng_val / 24,
+                stackgroup="one",
+                name="LNG Import",
+                mode="none",
+                fillcolor=FZJcolor.get("yellow")
+            )
+        )
+
+        fig.update_layout(
+            title=f"Erdgasimporte",
+            font=dict(size=16),
+            yaxis_title="Erdgas [TWh/h]",
+            legend=legend_dict,
+        )
+        st.plotly_chart(fig, use_container_width=True)
+
+
+        #%%
+        # Storage Charge and discharge
+        storage_operation = df.lngServed + df.pipeServed - total_demand_served
+        storage_discharge = [min(0, x) for x in storage_operation]
+        storage_charge = np.array([max(0, x) for x in storage_operation])
+
+        storage_operation_pl = df.pipeServed - total_demand_served
+        storage_charge_pl = np.array([max(0, x) for x in storage_operation_pl])
+
+        storage_operation_lng = storage_charge - storage_charge_pl
+        # storage_operation_lng = df.pipeServed - total_demand_served
+        storage_charge_lng = np.array([max(0, x) for x in storage_operation_lng])
+
+
+        fig = go.Figure()
+
+        fig.add_trace(
+            go.Scatter(
+                x=xvals,
+                y=storage_discharge,
+                stackgroup="two",
+                name="Ausspeicherung",
+                mode="none",
+                fillcolor=FZJcolor.get("red")
+            )
+        )
+
+        fig.add_trace(
+            go.Scatter(
+                x=xvals,
+                y=storage_charge_pl,
+                stackgroup="one",
+                name="Speicherung (Pipeline)",
+                mode="none",
+                fillcolor=FZJcolor.get("yellow")
+            )
+        )
+
+        fig.add_trace(
+            go.Scatter(
+                x=xvals,
+                y=storage_charge_lng,
+                stackgroup="one",
+                name="Speicherung (LNG)",
+                mode="none",
+                fillcolor=FZJcolor.get("blue3")
+            )
+        )
+
+        fig.update_layout(
+            title=f"Ein- und Ausspeicherung Gasspeicher",
+            font=dict(size=16),
+            yaxis_title="Erdgas [TWh/h]",
+            legend=legend_dict,
+        )
+
+        st.plotly_chart(fig, use_container_width=True)
